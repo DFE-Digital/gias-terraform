25d7df4e6f7425f69cf424a03110648619fdb01f
Matt Davies <matt@SL630798.AD.HQ.DEPT>
2026-08-24 09:23:09 +0100

    initial infra setup

---
+++ terraform/application/variables.tf
@@ -0,0 +1,64 @@
+variable "environment" {
+  type        = string
+  description = "Name of the deployed environment in AKS"
+}
+variable "azure_resource_prefix" {
+  type        = string
+  description = "Standard resource prefix. Usually s189t01 (test) or s189p01 (production)"
+}
+variable "config" {
+  type        = string
+  description = "Long name of the environment configuration, e.g. development, staging, production..."
+}
+variable "config_short" {
+  type        = string
+  description = "Short name of the environment configuration, e.g. dv, st, pd..."
+}
+variable "service_name" {
+  type        = string
+  description = "Full name of the service. Lowercase and hyphen separated"
+}
+variable "service_short" {
+  type        = string
+  description = "Short name to identify the service. Up to 6 charcters."
+}
+variable "deploy_azure_backing_services" {
+  type        = bool
+  default     = true
+  description = "Deploy real Azure backing services like databases, as opposed to containers inside of AKS"
+}
+variable "external_url" {
+  type        = string
+  default     = null
+  description = "Healthcheck URL for StatusCake uptime monitoring"
+}
+variable "apex_url" {
+  type        = string
+  default     = null
+  description = "URL for StatusCake SSL certificate monitoring. Only for DNS zone apex domain."
+}
+variable "statuscake_contact_groups" {
+  type        = list(any)
+  default     = []
+  description = "ID of the contact group in statuscake web UI"
+}
+variable "enable_monitoring" {
+  type        = bool
+  default     = false
+  description = "Enable monitoring and alerting"
+}
+
+variable "enable_logit" { default = true }
+
+variable "probe_path" {
+  type        = string
+  default     = "/healthcheck"
+  description = "Path for the liveness and startup probe. The probe can be disabled by setting this to null."
+}
+
+variable "redis_cache_capacity" { default = 1 }
+variable "redis_cache_family" { default = "C" }
+variable "redis_cache_sku_name" { default = "Standard" }
+variable "redis_queue_capacity" { default = 1 }
+variable "redis_queue_family" { default = "C" }
+variable "redis_queue_sku_name" { default = "Standard" }

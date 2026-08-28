9667f6c9dda01766a361ba4c96cbfaadee3d28c0
Philip Hope <philip.hope@mobilise.cloud>
2026-08-28 13:50:42 +0100

    2644 - Get the Terraform to successfully plan

--- terraform/application/terraform.tf
+++ terraform/application/terraform.tf
@@ -1,10 +1,13 @@
 terraform {
   required_version = "= 1.14.5"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "4.71.0"
+    }
+    statuscake = {
+      source  = "StatusCakeDev/statuscake"
+      version = "2.2.2"
     }
   }
   backend "azurerm" {
@@ -16,4 +19,8 @@ provider "azurerm" {
   features {}
 
   resource_provider_registrations = "none"
+}
+
+provider "statuscake" {
+  api_token = module.infrastructure_secrets.map.STATUSCAKE-API-TOKEN
 }

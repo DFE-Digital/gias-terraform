84433fa2f7d196f2447a499f8f49e66592e08585
Philip Hope <philip.hope@mobilise.cloud>
2026-09-09 08:34:26 +0100

    2644 - SQL fixes

--- terraform/application/sql.tf
+++ terraform/application/sql.tf
@@ -1,27 +1,43 @@
 module "sql-test" {
   source = "./vendor/modules/azure//azure/sql"
 
-  server_name_suffix    = "api"
+  server_name_suffix    = "edubase"
   environment           = var.environment
   azure_resource_prefix = var.azure_resource_prefix
   service_name          = var.service_name
   service_short         = var.service_short
   config_short          = var.config_short
 
-
-  azure_sql_sku                       = "BC_Gen5_2"
-  storage_account_type                = "Local"
-  lt_ret_pol_weekly_retention         = "P4W"
-  st_ret_pol_backup_interval_in_hours = 24
-  st_ret_pol_retention_days           = 14
-
-  # subnet_id    = module.network.sql_subnet
+  databases = {
+    edubase = {
+      server_name_suffix                  = "edubase"
+      azure_sql_sku                       = "BC_Gen5_2"
+      storage_account_type                = "Local"
+      lt_ret_pol_weekly_retention         = "P4W"
+      st_ret_pol_backup_interval_in_hours = 24
+      st_ret_pol_retention_days           = 14
+    }
+    sandbox = {
+      server_name_suffix                  = "edubase-sandbox1"
+      azure_sql_sku                       = "GP_Gen5_2"
+      storage_account_type                = "Local"
+      st_ret_pol_backup_interval_in_hours = 24
+    }
+  }
 
   private_endpoint_subnet_id = module.network.sql_subnet
   private_endpoints = {
     "dd-apimod" = {
-      # resource_id  = azurerm_mssql_server.main.id
-      # dns_zone_id  = module.network.sql_dns_zone_id
+      subresource = "sqlServer"
+      resource_id = "default"
+      dns_zone_id = module.network.sql_privdns_id
+    }
+    "dd-rest" = {
+      subresource = "sqlServer"
+      resource_id = "default"
+      dns_zone_id = module.network.sql_privdns_id
+    }
+    "provider" = {
       subresource = "sqlServer"
       resource_id = "default"
       dns_zone_id = module.network.sql_privdns_id
@@ -34,9 +50,86 @@ module "sql-test" {
       ip_address = "52.232.19.237"
     },
     {
-      name             = "AllowDevOps"
+      name       = "app-api-t1dv-edubase-02"
+      ip_address = "40.68.248.76"
+    },
+    {
+      name       = "app-api-t1dv-edubase-03"
+      ip_address = "40.68.253.160"
+    },
+    {
+      name       = "app-api-t1dv-edubase-04"
+      ip_address = "52.174.26.87"
+    },
+    {
+      name       = "app-api-t1dv-edubase-05"
+      ip_address = "40.68.253.49"
+    },
+    {
+      name       = "app-api-t1dv-edubase-06"
+      ip_address = "52.178.25.250"
+    },
+    {
+      name       = "app-api-t1dv-edubase-07"
+      ip_address = "40.68.253.41"
+    },
+    {
+      name       = "app-api-t1dv-edubase-08"
+      ip_address = "20.82.34.7"
+    },
+    {
+      name       = "app-api-t1dv-edubase-09"
+      ip_address = "20.82.34.92"
+    },
+    {
+      name       = "app-api-t1dv-edubase-10"
+      ip_address = "20.82.34.141"
+    },
+    {
+      name       = "app-api-t1dv-edubase-11"
+      ip_address = "20.82.34.156"
+    },
+    {
+      name       = "app-api-t1dv-edubase-12"
+      ip_address = "20.82.35.17"
+    },
+    {
+      name       = "app-api-t1dv-edubase-13"
+      ip_address = "20.82.35.148"
+    },
+    {
+      name       = "azuredevops-59489"
+      ip_address = "20.224.184.45"
+    },
+    {
+      name             = "DfEGlobalProtectVPNRange1"
       start_ip_address = "208.127.46.232"
       end_ip_address   = "208.127.46.239"
+    },
+    {
+      name             = "DfEGlobalProtectVPNRange2"
+      start_ip_address = "208.127.46.240"
+      end_ip_address   = "208.127.46.255"
+    },
+    {
+      name       = "Prisma Access 2"
+      ip_address = "208.127.45.248"
+    },
+    {
+      name       = "Prisma Access 5"
+      ip_address = "208.127.50.230"
+    },
+    {
+      name       = "Prisma Access 6"
+      ip_address = "208.127.52.236"
+    },
+    {
+      name       = "Prisma Access 8"
+      ip_address = "208.127.53.148"
+    },
+    {
+      name       = "TemporarilyAllowRegressionTestsPipelineIp"
+      ip_address = "68.221.179.161"
     }
   ]
 }
